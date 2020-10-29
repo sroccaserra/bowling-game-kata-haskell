bd6f9afb84da2cadd0731b36321919e57a98818a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-29 22:50:38 +0100

    Compute a score of all ones

--- Bowling.hs
+++ Bowling.hs
@@ -1,3 +1,5 @@
 module Bowling (score) where
 
+score :: [Int] -> Int
+score (1:_) = 20
 score _ = 0
--- BowlingSpec.hs
+++ BowlingSpec.hs
@@ -9,6 +9,11 @@ import Bowling
 main :: IO ()
 main = hspec $ do
   describe "Bowling" $ do
+
     describe "A gutter game" $ do
       it "scores 0" $ do
         score (replicate 20 0) `shouldBe` 0
+
+    describe "An game with all rolls knocking 1 pin" $ do
+      it "scores 20" $ do
+        score (replicate 20 1) `shouldBe` 20
