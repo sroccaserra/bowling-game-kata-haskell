757442382d76b8430b1eca228df0df91621d9408
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 21:11:00 +0100

    r Add isLastFrame function

--- Bowling.hs
+++ Bowling.hs
@@ -6,16 +6,22 @@ score = scoreRec 1
 scoreRec :: Int -> [Int] -> Int
 scoreRec _ [] = 0
 scoreRec _ [_] = div 1 0
-scoreRec 10 [x, y, z] = x + y + z
+scoreRec n [x, y, z]
+  | isLastFrame n =
+    x + y + z
 scoreRec n (x:y:z:rest)
   | isStrike x =
     10 + y + z + scoreRec (succ n) (y:z:rest)
   | isSpare x y =
     10 + z + scoreRec (succ n) (z:rest)
-scoreRec n (x:y:rest) = x + y + scoreRec (succ n) rest
+scoreRec n (x:y:rest) =
+  x + y + scoreRec (succ n) rest
 
 isStrike :: Int -> Bool
 isStrike = (==) 10
 
 isSpare :: Int -> Int -> Bool
 isSpare x y = x + y == 10
+
+isLastFrame :: Int -> Bool
+isLastFrame = (==) 10
