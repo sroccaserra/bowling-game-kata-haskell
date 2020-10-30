8cd3f9e77293ad476db9439a1577d48433238980
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 19:18:32 +0100

    r Simplify a clause

--- Bowling.hs
+++ Bowling.hs
@@ -7,8 +7,9 @@ scoreRec :: Int -> [Int] -> Int
 scoreRec _ [] = 0
 scoreRec _ [_] = div 1 0
 scoreRec 10 [x, y, z] = x + y + z
-scoreRec n (10:y:z:rest) =  10 + y + z + scoreRec (n + 1) (y:z:rest)
 scoreRec n (x:y:z:rest)
+  | x == 10 =
+    10 + y + z + scoreRec (n + 1) (y:z:rest)
   | x + y == 10 =
     10 + z + scoreRec (n + 1) (z:rest)
 scoreRec n (x:y:rest) = x + y + scoreRec (n + 1) rest
