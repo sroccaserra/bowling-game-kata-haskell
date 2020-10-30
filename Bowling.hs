bd0f110a6f575fd3c2c50542fa0d2fc1d9627343
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 19:06:08 +0100

    r Introduce frame number

--- Bowling.hs
+++ Bowling.hs
@@ -1,11 +1,14 @@
 module Bowling (score) where
 
 score :: [Int] -> Int
-score [] = 0
-score [_] = div 1 0
-score [x, y, z] = x + y + z
-score (10:y:z:rest) = 10 + y + z + score (y:z:rest)
-score (x:y:z:rest)
+score = scoreRec 1
+
+scoreRec :: Int -> [Int] -> Int
+scoreRec _ [] = 0
+scoreRec _ [_] = div 1 0
+scoreRec _ [x, y, z] = x + y + z
+scoreRec n (10:y:z:rest) =  10 + y + z + scoreRec (n + 1) (y:z:rest)
+scoreRec n (x:y:z:rest)
   | x + y == 10 =
-    10 + z + score (z:rest)
-score (x:y:rest) = x + y + score rest
+    10 + z + scoreRec (n + 1) (z:rest)
+scoreRec n (x:y:rest) = x + y + scoreRec (n + 1) rest
