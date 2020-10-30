faed69216a6b80f5e5f38a759149a7da1659a5ae
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 22:44:45 +0100

    r Use case of for matching

--- Bowling.hs
+++ Bowling.hs
@@ -4,18 +4,19 @@ score :: [Int] -> Int
 score = scoreRec 1
 
 scoreRec :: Int -> [Int] -> Int
-scoreRec _ [] = 0
-scoreRec n [x, y, z]
-  | isLastFrame n =
-    x + y + z
-scoreRec n (x:y:z:rest)
-  | isStrike x =
-    10 + y + z + scoreRec (succ n) (y:z:rest)
-  | isSpare x y =
-    10 + z + scoreRec (succ n) (z:rest)
-scoreRec n (x:y:rest) =
-  x + y + scoreRec (succ n) rest
-scoreRec _ [_] = error "Wrong number of rolls"
+scoreRec n xs = case xs of
+  [] -> 0
+  [x, y, z]
+    | isLastFrame n ->
+      x + y + z
+  x:y:z:rest
+    | isStrike x ->
+      10 + y + z + scoreRec (succ n) (y:z:rest)
+    | isSpare x y ->
+      10 + z + scoreRec (succ n) (z:rest)
+  x:y:rest ->
+    x + y + scoreRec (succ n) rest
+  [_] -> error "Wrong number of rolls"
 
 isStrike :: Int -> Bool
 isStrike =
