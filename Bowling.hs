f1451b71b1889c7b858da2cc0bcac4b1be8cc082
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-30 19:22:21 +0100

    r Add isStrike and isSpare functions

--- Bowling.hs
+++ Bowling.hs
@@ -8,8 +8,14 @@ scoreRec _ [] = 0
 scoreRec _ [_] = div 1 0
 scoreRec 10 [x, y, z] = x + y + z
 scoreRec n (x:y:z:rest)
-  | x == 10 =
+  | isStrike x =
     10 + y + z + scoreRec (n + 1) (y:z:rest)
-  | x + y == 10 =
+  | isSpare x y =
     10 + z + scoreRec (n + 1) (z:rest)
 scoreRec n (x:y:rest) = x + y + scoreRec (n + 1) rest
+
+isStrike :: Int -> Bool
+isStrike = (==) 10
+
+isSpare :: Int -> Int -> Bool
+isSpare x y = x + y == 10
