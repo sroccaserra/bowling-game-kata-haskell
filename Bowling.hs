43a4140d96c96440fcf28c3ebc791663d761f6b9
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-31 01:58:22 +0100

    r Rename next_n to n'

--- Bowling.hs
+++ Bowling.hs
@@ -10,14 +10,14 @@ score' n xs = case xs of
     | isLastFrame n ->
       x + y + z
     | isStrike x ->
-      10 + y + z + score' next_n (y:z:rest)
+      10 + y + z + score' n' (y:z:rest)
     | isSpare x y ->
-      10 + z + score' next_n (z:rest)
+      10 + z + score' n' (z:rest)
   x:y:rest ->
-    x + y + score' next_n rest
+    x + y + score' n' rest
   [_] -> error "Wrong number of rolls"
   where
-    next_n = succ n
+    n' = succ n
     isStrike = (==) 10
     isSpare x y = x + y == 10
     isLastFrame = (==) 10
