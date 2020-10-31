dfb0ee0801082e9695070cf8b33e3c3ff50d8b3c
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-10-31 01:37:38 +0100

    r Extract variable next_n

--- Bowling.hs
+++ Bowling.hs
@@ -11,13 +11,14 @@ score' n xs = case xs of
       x + y + z
   x:y:z:rest
     | isStrike x ->
-      10 + y + z + score' (succ n) (y:z:rest)
+      10 + y + z + score' next_n (y:z:rest)
     | isSpare x y ->
-      10 + z + score' (succ n) (z:rest)
+      10 + z + score' next_n (z:rest)
   x:y:rest ->
-    x + y + score' (succ n) rest
+    x + y + score' next_n rest
   [_] -> error "Wrong number of rolls"
   where
+    next_n = succ n
     isStrike = (==) 10
     isSpare x y = x + y == 10
     isLastFrame = (==) 10
