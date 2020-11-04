6ba5ba6bf10bb9ff0add7863bfe0de5076b03dee
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-11-04 20:09:59 +0100

    r Define Frame type

--- Bowling.hs
+++ Bowling.hs
@@ -2,24 +2,28 @@ module Bowling (score, Roll(..)) where
 
 data Roll = Roll Int
 
-score :: [Roll] -> Int
-score = score' 1
+data Frame = Frame Int
 
-score' :: Int -> [Roll] -> Int
-score' n xs = case xs of
+next :: Frame -> Frame
+next (Frame n) = Frame (n + 1)
+
+score :: [Roll] -> Int
+score = score' $ Frame 1
+
+score' :: Frame -> [Roll] -> Int
+score' f xs = case xs of
   [] -> 0
   Roll x:Roll y:Roll z:rest
-    | isLastFrame n ->
+    | isLastFrame f ->
       x + y + z
     | isStrike x ->
-      10 + y + z + score' n' (Roll y:Roll z:rest)
+      10 + y + z + score' (next f) (Roll y:Roll z:rest)
     | isSpare x y ->
-      10 + z + score' n' (Roll z:rest)
+      10 + z + score' (next f) (Roll z:rest)
   Roll x: Roll y:rest ->
-    x + y + score' n' rest
+    x + y + score' (next f) rest
   [_] -> error "Wrong number of rolls"
   where
-    n' = succ n
     isStrike = (==) 10
     isSpare x y = x + y == 10
-    isLastFrame = (==) 10
+    isLastFrame (Frame n)= n == 10
